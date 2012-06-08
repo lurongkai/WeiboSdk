c5b8b46cf938e1896436117798ca0e857c66b404
SND\Xuanchenlin_cp <SND\Xuanchenlin_cp@ddf9c79f-3887-47c2-86f8-040505c24a32>
2012-06-08 10:06:37 +0000

    git-svn-id: https://weibosdk.svn.codeplex.com/svn/SinaWeiboSDK_V2@16903 ddf9c79f-3887-47c2-86f8-040505c24a32

--- NetDimension.Weibo/Newtonsoft.Json/Utilities/CollectionUtils.cs
+++ NetDimension.Weibo/Newtonsoft.Json/Utilities/CollectionUtils.cs
@@ -1,4 +1,4 @@
-#region License
+ï»¿#region License
 // Copyright (c) 2007 James Newton-King
 //
 // Permission is hereby granted, free of charge, to any person
@@ -316,7 +316,7 @@ namespace Newtonsoft.Json.Utilities
     /// <param name="list">A sequence in which to locate a value.</param>
     /// <param name="value">The object to locate in the sequence</param>
     /// <param name="comparer">An equality comparer to compare values.</param>
-    /// <returns>The zero-based index of the first occurrence of value within the entire sequence, if found; otherwise, –1.</returns>
+    /// <returns>The zero-based index of the first occurrence of value within the entire sequence, if found; otherwise, ?.</returns>
     public static int IndexOf<TSource>(this IEnumerable<TSource> list, TSource value, IEqualityComparer<TSource> comparer)
     {
       int index = 0;
